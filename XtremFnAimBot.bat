91b23f6d8d9e15c8bdb5f6364817f99c8743377f
T̷̊̕R̷̎̍Ä̵́̓Ý̴͝X̶͌̓I̸͂͆ ⁶₆⁷ <121296534+Trayxi@users.noreply.github.com>
2023-10-09 18:35:27 +0200

    Add files via upload

---
+++ XtremFnAimBot.bat
@@ -0,0 +1,10 @@
+dim speechobject
+set speechobject=createobject("sapi.spvoice")
+
+speechobject.speak "PC HACKED"
+x=msgbox("PC HACK, appuyez sur OK pour effacer toutes les données de votre ordinateur", 16, "HACKED")
+x=msgbox("VOTRE PC VA EXPLOSER DANS 5...", 16, "KABOUM")
+
+Do While 1<2
+speechobject.speak "WARNING, PC HACKED, DELETING FILES. UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU, Oh shit, here we go again"
+Loop
